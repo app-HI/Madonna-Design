c34a06c48eb7a66cfcac4208b8622657f1dc09f4
aminhaghi86 <amin.haghi@hyperisland.se>
2023-05-18 11:22:32 +0200

    add scroll triger to video section

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
+gsap.registerPlugin(ScrollTrigger);
+
 gsap.set(".h1-main", {
-    opacity: 0,
-  });
+  opacity: 0,
+});
 gsap.set(".item", {
-    opacity: 0,
-  });
-  gsap.to(".item", {
-    duration: 0.1,
-    delay: 1,
-    ease: "power1",
-    opacity: 1,
-    stagger: {
-      each: 0.25,
-      from: "start",
-    },
-    onComplete: function() {
-      gsap.to(".h1-main", {
-        opacity: 1,
-        repeat: 1,
-      });
-    }
-  });
-  
+  opacity: 0,
+});
+gsap.to(".item", {
+  duration: 0.1,
+  delay: 1,
+  ease: "power1",
+  opacity: 1,
+  stagger: {
+    each: 0.25,
+    from: "start",
+  },
+  onComplete: function () {
+    gsap.to(".h1-main", {
+      opacity: 1,
+      repeat: 1,
+    });
+  },
+});
+
+//
+gsap.set(".video-item", {
+  opacity: 0,
+  y: 90,
+});
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".video-item", {
+  opacity: 1,
+  y: 0,
+  duration: 2,
+  stagger: 0.5,
+  scrollTrigger: {
+    trigger: "#video-section",
+    start: "top 80%",
+    end: "bottom 20%",
+    toggleActions: "play none none reverse",
+  },
+});
